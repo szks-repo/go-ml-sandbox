77dab917f8b4eb47d1df83ee32bef239c1977ab7
szks-repo <szkscookie@gmail.com>
2024-09-15 23:42:58 +0900

    add: scaling

--- go/math/main.go
+++ go/math/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"strings"
 )
 
 // 母分散σ**2
-func getVariance(input []int) float64 {
+func getVariance(input []float64) float64 {
 	if len(input) == 0 {
 		return 0.0
 	}
@@ -17,14 +18,14 @@ func getVariance(input []int) float64 {
 
 	var total float64
 	for _, n := range input {
-		total += math.Pow(float64(n)-AVE, 2)
+		total += math.Pow(n-AVE, 2)
 	}
 
 	return total * (1 / float64(N))
 }
 
 // 標本分散
-func getSampleVariance(input []int) float64 {
+func getSampleVariance(input []float64) float64 {
 	if len(input) == 0 {
 		return 0.0
 	}
@@ -41,16 +42,16 @@ func getSampleVariance(input []int) float64 {
 }
 
 // 標準偏差σ
-func getSigma(input []int) float64 {
+func getSigma(input []float64) float64 {
 	return math.Sqrt(getVariance(input))
 }
 
-func getAverage(input []int) float64 {
+func getAverage(input []float64) float64 {
 	if len(input) == 0 {
 		return 0.0
 	}
 
-	var total int
+	var total float64
 	for _, v := range input {
 		total += v
 	}
@@ -58,18 +59,42 @@ func getAverage(input []int) float64 {
 	return float64(total) / float64((len(input)))
 }
 
+func getScaled(inputs []float64) []float64 {
+	ave := getAverage(inputs)
+	sigma := getSigma(inputs)
+
+	result := make([]float64, len(inputs))
+	for i := range inputs {
+		result[i] = (float64(inputs[i]) - ave) / sigma
+	}
+
+	return result
+}
+
 func main() {
 	{
-		group := []int{-2, -1, 0, 1, 2}
+		group := []float64{-2, -1, 0, 1, 2}
 		fmt.Println("平均", getAverage(group))
 		fmt.Println("母分散", getVariance(group))
 		fmt.Println("標準偏差:", getSigma(group))
 	}
-	fmt.Println(strings.Repeat("*", 100))
 	{
-		group := []int{-4, -2, 0, 2, 4}
+		group := []float64{-4, -2, 0, 2, 4}
 		fmt.Println("平均", getAverage(group))
 		fmt.Println("母分散", getVariance(group))
 		fmt.Println("標準偏差:", getSigma(group))
 	}
+
+	for range 10 {
+		values := make([]float64, 10)
+		for i := 0; i < len(values); i++ {
+			values[i] = rand.Float64()
+		}
+
+		scaled := getScaled(values)
+		fmt.Println("scaled ages:", scaled)
+		fmt.Println("scaled sigma:", getSigma(scaled), math.Round(getSigma(scaled)))
+		fmt.Println(strings.Repeat("*", 100))
+	}
+
 }
